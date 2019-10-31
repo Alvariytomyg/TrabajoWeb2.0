ae2616aa267483509aa8287a9d0ce165a9204b59
programador <programador@VH203-27.upc.edu.pe>
2019-10-30 21:14:28 -0500

    First commit

---
+++ Artistfy/src/test/java/pe/edu/upc/webdevs/ArtistfyApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.webdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

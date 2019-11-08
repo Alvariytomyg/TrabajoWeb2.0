e9114421b2948ddbdfb4cf11cdd77aff80b94487
jhosset1998avalos <54128485+jhosset1998avalos@users.noreply.github.com>
2019-11-08 13:10:45 -0500

    Repository package advance

---
+++ Artistfy/src/main/java/pe/edu/upc/webdevs/model/Person.java
@@ -0,0 +1,88 @@
+package pe.edu.upc.webdevs.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="persons")
+public class Person implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+    
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name="first_name",nullable=false)
+	private String firstName;
+	
+	@Column(name="last_name",nullable=false)
+	private String lastName;
+	
+	@Column(name="dni",nullable=false)
+	private Long dni;
+	
+	@Column(name="email",nullable=false)
+	private String email;
+	
+	@Column(name="phone",nullable=false)
+	private Long phone;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Long getDni() {
+		return dni;
+	}
+
+	public void setDni(Long dni) {
+		this.dni = dni;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Long phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+	
+}

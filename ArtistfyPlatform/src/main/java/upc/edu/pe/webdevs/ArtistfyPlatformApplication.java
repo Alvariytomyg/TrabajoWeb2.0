0411324753caaad4b664fd4c9713f4da94580e51
alumnos <alumnos@VH203-28.upc.edu.pe>
2019-11-13 20:47:29 -0500

    Proyect Artistfy

---
+++ ArtistfyPlatform/src/main/java/upc/edu/pe/webdevs/ArtistfyPlatformApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.webdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistfyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistfyPlatformApplication.class, args);
+	}
+
+}

2495b08b28d230d59ac05a7c66fad660fe9ec476
alumnos <alumnos@VH203-28.upc.edu.pe>
2019-11-13 22:02:48 -0500

    validations when creating event

---
+++ ArtistfyPlatform/src/main/java/upc/edu/pe/webdevs/service/IPersonService.java
@@ -0,0 +1,7 @@
+package upc.edu.pe.webdevs.service;
+
+import upc.edu.pe.webdevs.model.Person;
+
+public interface IPersonService extends CrudService<Person>{
+
+}
---
+++ ArtistfyPlatform/src/test/java/upc/edu/pe/webdevs/ArtistfyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.webdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistfyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

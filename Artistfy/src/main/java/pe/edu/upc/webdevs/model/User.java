d118db2c4d3e83a0f9cdd84538527b0c1c6060fc
shey1922 <u201619750@upc.edu.pe>
2019-11-13 23:12:08 -0500

    create user

--- Artistfy/src/main/java/pe/edu/upc/webdevs/model/User.java
+++ Artistfy/src/main/java/pe/edu/upc/webdevs/model/User.java
@@ -22,4 +22,28 @@ private Integer id;
 private String username;
 @Column(name="password",nullable=false)
 private String password;
+
+public Integer getId() {
+	return id;
+}
+
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+	
+}
+public String getPassword() {
+	return username;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+	
+}
 }
